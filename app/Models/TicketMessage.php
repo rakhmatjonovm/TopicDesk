eabfadaf8e6562f134a8f245b4d40edeb9c6df72
rakhmatjonovm <mrjianarchist@gmail.com>
2025-11-30 18:26:23 +0500

    добавил модель хранения сообщений обращения

---
+++ app/Models/TicketMessage.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TicketMessage extends Model
+{
+    protected $fillable = [
+        'ticket_id',
+        'direction',
+        'content',
+        'user_message_id',
+        'support_message_id',
+        'payload',
+    ];
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+}
